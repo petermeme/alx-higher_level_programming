40357600848fac8fa4a9cf707bad1a8f695400c2
petermeme <petermeme90@gmail.com>
2023-08-09 15:09:56 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(0, 8):
     for num2 in range(num + 1, 10):
-         print("{}{}".format(num, num2), end=", ") 
+        print("{}{}".format(num, num2), end=", ")
 print("{}{}".format(num + 1, num2))
