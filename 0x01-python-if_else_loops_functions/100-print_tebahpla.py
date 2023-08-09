e7f64dabf0740d39d54bc7d4314a316461eaf654
petermeme <petermeme90@gmail.com>
2023-08-09 16:42:49 -0700

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 
 # Print the alphabet in lowercase, not followed by a new line.
 
-for alphabet in(reversed (range(97, 123))):
+for alphabet in (reversed(range(97, 123))):
     if alphabet % 2 != 0:
         alphabet = alphabet - 32
     print("{:c}".format(alphabet), end="")
