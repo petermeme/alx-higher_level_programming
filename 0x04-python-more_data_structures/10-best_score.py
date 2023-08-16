0e1d3837024d219191a9d1b068bb4ccf36e78f80
petermeme <petermeme90@gmail.com>
2023-08-16 15:07:32 -0700

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    if not a_dictionary:
+        return None
+    keys = list(a_dictionary.keys())
+    values = list(a_dictionary.values())
+    return keys[values.index(max(values))]
