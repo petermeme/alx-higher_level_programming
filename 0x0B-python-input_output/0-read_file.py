2efb98a51c6b2c99b6579788f1a3b8d288d5df66
petermeme <petermeme90@gmail.com>
2023-09-13 09:24:51 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+a function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
 def read_file(filename=""):
     """function that reads a text file and prints it"""
 
