def033f379d7dabb9d6104352f41599e94fd741e
petermeme <petermeme90@gmail.com>
2023-09-13 09:31:46 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""
+function that writes a string to a text file (UTF8)
+"""
+
+
 def write_file(filename="", text=""):
     """function that writes a string to a text file and return the number
     of characters written"""
 
-    with open(filename, 'w') as f:
+    with open(filename, 'w', encoding="UTF-8") as f:
         return f.write(text)
