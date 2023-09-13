9e43522373b2ec3fb75097c424c99c88455d74cf
petermeme <petermeme90@gmail.com>
2023-09-13 09:35:28 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+function that appends a string at the end of a text file (UTF8)
+"""
+
+
 def append_write(filename="", text=""):
     """function that appends a string at the end of a text file and return
     the number of characters added"""
