192473a9f94fb2bb2c5646e61c427f8c7a6cab5e
petermeme <petermeme90@gmail.com>
2023-11-19 10:56:02 -0800

    0x0F-python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 #code should not be executed when imported
 if __name__ == "__main__":
     #script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     #Results must be sorted in ascending order by states.id
     cur.execute("SELECT * FROM states ORDER BY id")
