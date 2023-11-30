c3c7d7f526d21980c6efcb416cb948eb3df8c220
petermeme <petermeme90@gmail.com>
2023-11-30 12:05:08 -0800

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""must contain the function"""
+
+
+def find_peak(list_of_integers):
+    max_i = None
+    for ele in list_of_integers:
+        if max_i is None or max_i < ele:
+            max_i = ele
+    return max_i
