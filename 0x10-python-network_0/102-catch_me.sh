cc938694283024f132dda6abedcec2ce3549d095
petermeme <petermeme90@gmail.com>
2023-11-30 12:35:03 -0800

    advanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that makes a request to 0.0.0.0:5000/catch_me
-curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You got me!" -L -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
