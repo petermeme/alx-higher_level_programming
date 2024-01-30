0a1023b99672b40618d926d7ad00d6178925d996
petermeme <petermeme90@gmail.com>
2024-01-30 23:33:42 +0300

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-request(starWarsUri, function (_err, _res, body) {
-  body = JSON.parse(body);
-  console.log(body.title);
+// Check if the correct number of arguments is provided
+if (process.argv.length !== 3) {
+  console.error('Usage: ./3-starwars_title.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+// Make a GET request to the Star Wars API
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error('Failed to retrieve movie information. Status code:', response.statusCode);
+  } else {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  }
 });
